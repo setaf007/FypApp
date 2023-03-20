7b1578760e62caf68fc8c56caff30d5d8658f35c
NewArk117 <124663457+NewArk117@users.noreply.github.com>
2023-03-20 21:46:55 +0800

    Update main.py

--- FypApp/DataStorage/main.py
+++ FypApp/DataStorage/main.py
@@ -3,21 +3,8 @@ import requests
 import json
 import csv
 import os
+import glob
 from datetime import date
-
-"""
-print("test mi")
-url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=c31b6e2d6116221237f8f644e3c95605'
-print(url)
-
-res = requests.get(url)
-json_it = res.json()
-
-df = pd.json_normalize(json_it)
-
-#writing data in csv format
-df.to_csv("test.csv")
-"""
 
 # get today date
 today_is = date.today().strftime("%Y-%m-%d")
@@ -30,19 +17,34 @@ save_dir = "/app/data"
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
-country = ["australia", "singapore", "malaysia"]
+country = ["australia", "singapore"]
 
 for i in range(len(country)):
     # Set up the API endpoint URL
-    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + country[i] + "/" + today_is + "/" + today_is + "?unitGroup=metric&include=days&key=8MJQ4966D6CWWU5GBV7KBHBDS&contentType=csv"
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + country[i] + "/" + "2022-06-01" + "/" + today_is + "?unitGroup=metric&include=days&key=8MJQ4966D6CWWU5GBV7KBHBDS&contentType=csv"
     #print(url) #test code
-    df = pd.read_csv(url)
 
-    # set up the path to save the csv files
-    filePath = os.path.join(save_dir, "data_"+country[i]+".csv")
+    try:
+        df=pd.read_csv(url)
 
-    # save the csv files
-    df.to_csv(filePath, index=False)
+        # set up the path to save the csv files
+        filePath = os.path.join(save_dir, "data_"+country[i]+".csv")
 
-    #df.to_csv("data_"+country[i]+".csv")
-    #print("data_"+country[i]+".csv") #test code
+        # save the csv files
+        df.to_csv(filePath, index=False)
+        #print("data_"+country[i]+".csv") #test code
+
+    except Exception as e:
+        print(f"Error occurred while processing {country[i]}: {e}")
+
+#concat
+os.chdir("/app/data")
+
+extension = "csv"
+all_filenames = [i for i in
+    glob.glob('*.{}'.format(extension))]
+
+#combine all files in the llist
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+#export to csv
+combined_csv.to_csv("combined_csv.csv", index=False, encoding='utf-8-sig')
